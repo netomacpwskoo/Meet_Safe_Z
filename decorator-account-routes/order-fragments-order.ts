81481aae40a8cd19a86ea9dee67a749f86dc9445
netomacpwskoo <netomacpwskoo@users.noreply.github.com>
2025-11-03 14:18:39 +0000

    Add order-fragments-order.ts, src.styl, base.tsx

---
+++ decorator-account-routes/order-fragments-order.ts
@@ -0,0 +1,4 @@
+export const config = { runtime: 'edge' };
+
+export default function frontend_user_export() { return <div>Component</div>; }
+
---
+++ decorator-account-routes/static-validator-email/base.tsx
@@ -0,0 +1,4 @@
+import { ethers } from 'ethers';
+
+export default function local_email_controller() { return <div>Component</div>; }
+
